4c07270375ec98654c0d267bf774e966cd84788f
Ward <michael.ward06@sap.com>
2020-04-21 14:24:22 -0600

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+echo 'export PATH="/usr/local/opt/python/libexec/bin:$PATH"' ~/.profile
+
+brew install wget
+brew install awscli
+brew install python
+brew install jq
+brew install kubernetes-cli
+brew install kops
+brew cask install atom
+brew install git-crypt
+
+apm install git-language
+apm install linter-yaml
+apm install language-docker
+apm install rulerz
